3d829db2d72cfada83af01bfb7d04f719171d7d9
bschack <benharris1102@gmail.com>
2025-02-11 13:36:12 -0500

    Initial Commit

---
+++ VirusTotalMenuBar/VirusTotalMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirusTotalMenuBarApp.swift
+//  VirusTotalMenuBar
+//
+//  Created by Ben Schack on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VirusTotalMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirusTotalMenuBarTests/VirusTotalMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  VirusTotalMenuBarTests.swift
+//  VirusTotalMenuBarTests
+//
+//  Created by Ben Schack on 2/11/25.
+//
+
+import Testing
+@testable import VirusTotalMenuBar
+
+struct VirusTotalMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
